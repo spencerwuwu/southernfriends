21f9cc79befead01368eb7fcb32f592366581f9f
Wei-Cheng Wu <spencerwu85@gmail.com>
2017-02-24 02:20:45 +0800

    Finish Ch3-A

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # southernfriends
 
 02.22	1.5hr		init
+02.23	3.0hr		Finish Ch3-A
---
+++ converter.js
@@ -0,0 +1,12 @@
+fs = require('fs');
+fs.readFile('./A-content/A8.md', 'utf8',function(err,data){
+	if (err) {
+		return console.log(err);
+	}
+	var showdown  = require('showdown'),
+	converter = new showdown.Converter(),
+	html      = converter.makeHtml(data);
+
+	console.log(html);
+});
+
