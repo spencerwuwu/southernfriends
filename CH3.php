a1d617b5604237d4cf23a085cc1f4ba0c538d9a6
Wei-Cheng Wu <spencerwu85@gmail.com>
2017-03-02 23:13:24 +0800

    Try php

---
+++ CH3.php
@@ -0,0 +1,40 @@
+<?php
+$top = file_get_contents('basic.php');
+echo $top;
+?>
+
+        <div class="one item content">
+          <h2>A<br>聘僱篇</h2>
+          <img src="img/Ch3/A.png" />
+        </div>
+
+        <div class="item list-con">
+          <div class="item list-small heavy">
+            <a href="CH3_A1.html">A1</a>
+          </div>
+          <div class="item list-small heavy">
+            <a href="CH3_A2.html">A2</a>
+          </div>
+          <div class="item list-small heavy">
+            <a href="CH3_A3.html">A3</a>
+          </div>
+          <div class="item list-small heavy">
+            <a href="CH3_A4.html">A4</a>
+          </div>
+          <div class="item list-small heavy">
+            <a href="CH3_A5.html">A5</a>
+          </div>
+          <div class="item list-small heavy">
+            <a href="CH3_A6.html">A6</a>
+          </div>
+          <div class="item list-small heavy">
+            <a href="CH3_A7.html">A7</a>
+          </div>
+          <div class="item list-small heavy">
+            <a href="CH3_A8.html">A8</a>
+          </div>
+        </div>
+<?php
+$end = file_get_contents('end.php');
+echo $end;
+?>
--- basic.php
+++ basic.php
@@ -1,3 +1,6 @@
+<?php
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -96,41 +99,3 @@
 
     <div class="topcon">
       <div class="myrow centered">
-
-        <div class="one item content">
-          <h2>A<br>聘僱篇</h2>
-          <img src="img/Ch3/A.png" />
-        </div>
-
-        <div class="item list-con">
-          <div class="item list-small heavy">
-            <a href="CH3_A1.html">A1</a>
-          </div>
-          <div class="item list-small heavy">
-            <a href="CH3_A2.html">A2</a>
-          </div>
-          <div class="item list-small heavy">
-            <a href="CH3_A3.html">A3</a>
-          </div>
-          <div class="item list-small heavy">
-            <a href="CH3_A4.html">A4</a>
-          </div>
-          <div class="item list-small heavy">
-            <a href="CH3_A5.html">A5</a>
-          </div>
-          <div class="item list-small heavy">
-            <a href="CH3_A6.html">A6</a>
-          </div>
-          <div class="item list-small heavy">
-            <a href="CH3_A7.html">A7</a>
-          </div>
-          <div class="item list-small heavy">
-            <a href="CH3_A8.html">A8</a>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-
-  </body>
