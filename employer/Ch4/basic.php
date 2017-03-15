0611b67f3ddc7211260e9612324984916fdc3b18
Wei-Cheng Wu <spencerwu85@gmail.com>
2017-03-15 23:20:33 +0800

    Add each basic

---
+++ employer/Ch4/basic.php
@@ -0,0 +1,116 @@
+<?php
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name=viewport content="width=device-width, initial-scale=1, maximum-scale=1.0">
+
+    <title> 南方朋友 | Southern Friends </title>
+
+
+    <link rel="stylesheet" type="text/css" href="../static/css/basic.css">
+    <link rel="stylesheet" type="text/css" href="../static/css/myresponsive.css">
+    <script src="../static/js/jquery.min.js"></script>
+    <script src="../static/js/basic.js"></script>
+  </head>
+  <body>
+    <!-- side menu bar -->
+    <div class="narrow menu-slideOut" id="menu-side">
+      <div class="myrow center">
+
+        <div class="one item menu-main topic3 heavy mywhite" id="menu-main1">
+          <p>&#8227;&nbsp;雇主小幫手</p>
+        </div>
+
+        <div id="menu-content1" class="menu-content-con">
+        <div class="one item menu-content" >
+          <div class=" menu-n-text mytext heavy mywhite short">
+            <p>Ch1 移工國簡介</p>
+          </div>
+          <div class="menu-n-text mytext mywhite ">
+            <a href="../Ch1/in.php">印尼簡介</a>
+          </div>
+          <div class="menu-n-text mytext mywhite ">
+            <a href="../Ch1/phi.php">菲律賓簡介</a>
+          </div>
+          <div class="menu-n-text mytext mywhite ">
+            <a href="../Ch1/tai.php">泰國簡介</a>
+          </div>
+          <div class="menu-n-text mytext mywhite ">
+            <a href="../Ch1/vie.php">越南簡介</a>
+          </div>
+        </div>
+
+        <div class="one item menu-content">
+          <div class=" menu-n-text mytext heavy mywhite short">
+            <p>Ch2 僱用外籍勞工須知</p>
+          </div>
+          <div class=" menu-n-text mytext mywhite ">
+            <a href="../Ch2/CH2.php">聘顧需知</a>
+          </div>
+        </div>
+
+
+        <div class="one item menu-content">
+          <div class=" menu-n-text mytext heavy mywhite short">
+            <p>Ch3 法律案例</p>
+          </div>
+          <div class="menu-n-text mytext mywhite ">
+            <a href="../Ch3/CH3_A.php">A 聘顧篇</a>
+          </div>
+          <div class="menu-n-text mytext mywhite ">
+            <a href="../Ch3/CH3_B.php">B 指/調派工作篇</a>
+          </div>
+          <div class="menu-n-text mytext mywhite ">
+            <a href="../Ch3/CH3_C.php">C 行政流程篇</a>
+          </div>
+        </div>
+
+
+        <div class="one item menu-content">
+          <div class=" menu-n-text mytext heavy mywhite short">
+            <p>Ch4 附錄</p>
+          </div>
+          <div class="menu-n-text mytext mywhite ">
+            <a href="CH4.php">附錄</a>
+          </div>
+<!--
+          <div class="menu-n-text mytext mywhite ">
+            <a href="CH3_B.php">B 指/調派工作篇</a>
+          </div>
+          <div class="menu-n-text mytext mywhite ">
+            <a href="CH3_C.php">C 行政流程篇</a>
+          </div>
+-->
+        </div>
+        </div>
+
+
+        <div class="one item menu-main topic3 heavy mywhite" id="menu-main2">
+          <p>&#8227;&nbsp;移工小幫手</p>
+        </div>
+
+        <div class="one item menu-main topic3 heavy mywhite" id="menu-main3">
+          <p>&#8227;&nbsp;直聘小幫手</p>
+        </div>
+
+      </div>
+    </div>
+    <div id="menublack"></div>
+
+    <div class="narrow menubar-s">
+      <div class="topic2 heavy mygrey short" id="menubtn"><a>&equiv;</a></div>
+    </div>
+
+    <div class="menu-bar">
+    </div>
+
+    <!-- end of basic -->
+
+
+
+    <div class="topcon">
+      <div class="myrow centered">
