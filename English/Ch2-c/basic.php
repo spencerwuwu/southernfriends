897793ee0e1814b0eaf1e754a1498bde8b67de87
Wei-Cheng Wu <spencerwu85@gmail.com>
2017-03-23 04:16:19 +0800

    Upload

--- English/Ch2-c/basic.php
+++ English/Ch2-c/basic.php
@@ -18,7 +18,57 @@
   </head>
   <body>
 	<!-- side menu bar -->
-	
+		<!-- side menu bar -->
+	<div class="narrow menu-slideOut" id="menu-side">
+	  <div class="myrow center">
+
+		
+		<div class="one item menu-main topic3 heavy mywhite" id="menu-main2">
+		  <p>&#8227;&nbsp;移工小幫手</p>
+		</div>
+
+		<div id="menu-content2" class="menu-content-con">
+		<div class="one item menu-content" >
+		  <div class=" menu-n-text mytext heavy mywhite">
+            <a href="../Ch1-c/Ch1.php">ch1 關於台灣</a>
+		  </div>
+		  <div class=" menu-n-text mytext heavy mywhite">
+            <a href="Ch2.php">ch2 語言小教室</a>
+		  </div>
+		  <div class=" menu-n-text mytext heavy mywhite">
+            <a href="../Ch3-c/Ch3.php">ch3 假日好去處</a>
+		  </div>
+		  <div class=" menu-n-text mytext heavy mywhite">
+            <a href="../Ch4-c/Ch4.php">ch4 宗教小教室</a>
+		  </div>
+		  <div class=" menu-n-text mytext heavy mywhite">
+            <a href="../Ch5-c/Ch5.php">ch5 交通與生活小知識</a>
+		  </div>
+		  <div class=" menu-n-text mytext heavy mywhite">
+            <a href="../Ch6-c/Ch6.php">ch6 你一定要知道的那些事</a>
+		  </div>
+		  <div class=" menu-n-text mytext heavy mywhite">
+            <a href="../Ch7-c/Ch7.php">ch7 為國際移工舉辦的節日</a>
+		  </div>
+		  <div class=" menu-n-text mytext heavy mywhite">
+            <a href="../Ch8-c/Ch8.php">ch8 外國廣播節目</a>
+		  </div>
+		</div>
+		</div>
+
+
+	  </div>
+	</div>
+	<div id="menublack"></div>
+
+	<div class="narrow menubar-s">
+	  <div class="topic2 heavy mygrey short" id="menubtn"><a>&equiv;</a></div>
+	</div>
+
+	<div class="menu-bar">
+	</div>
+
+	<!-- end of basic -->
 
 	<!-- end of basic -->
     <div class="topcon">
