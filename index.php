b95f438c396baf7118d1a2b626f3f7578d7d9e0e
Wei-Cheng Wu <spencerwu85@gmail.com>
2017-03-23 01:13:34 +0800

    Update

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 $top = file_get_contents('basic.php');
 echo $top;
 ?>
-<div class="topcon">
+<div class="topcon top-addon">
       <div class="myrow centered">
-        <div class="one item content">
+        <div class="item main-content">
           <h2>南方朋友</h2>
           <img src="首頁.png" />
         </div>
