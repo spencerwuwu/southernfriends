b090e79e737055915eedcf1c26dff1e6d2f34e3c
Wei-Cheng Wu <spencerwu85@gmail.com>
2017-05-06 18:46:26 +0800

    Fix index responsive

--- index.php
+++ index.php
@@ -3,7 +3,27 @@ $top = file_get_contents('basic.php');
 echo $top;
 ?>
 <div class="topcon top-addon">
-      <div class="myrow centered">
+      <div class="wide myrow centered">
+        <div class="item mainextra-content">
+          <h2>南方朋友</h2>
+          <img src="首頁.png" />
+        </div>
+      </div>
+      <div class="item list-con">
+        <div class="item list-e heavy">
+          <a href="files/employer1.swf">雇主小幫手</a>
+        </div>
+        <div class="item list-e heavy">
+          <a href="files/employer1.swf">移工小幫手</a>
+        </div>
+        <div class="item list-e heavy">
+          <a href="files/employer1.swf">直聘小幫手</a>
+        </div>
+        <div class="item list-e heavy">
+          <a href="download/index.php">下載專區</a>
+        </div>
+      </div>
+      <div class="narrow myrow centered">
         <div class="item mainindex-content">
           <h2>南方朋友</h2>
           <img src="首頁.png" />
